124fe7a4c35e6a86fddc087ccbf122e870896ffc
Maknoob <imak90@gmail.com>
2024-07-15 09:26:04 +0200

    Refactored search methods

--- repositories/src/main/java/com/codingmak/repositories/SpellRepository.java
+++ repositories/src/main/java/com/codingmak/repositories/SpellRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SpellRepository extends JpaRepository<Spell, Long> {
-    List<Spell> findByName(String name);
     List<Spell> findByType(String type);
-    List<Spell> findByBonus(String bonus);
     List<Spell> findByNameAndType(String name, String bonus);
+
+    List<Spell> findByNameContaining(String name);
+
+    List<Spell> findByBonusContaining(String bonus);
 }
