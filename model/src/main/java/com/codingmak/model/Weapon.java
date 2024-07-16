87425bf5433ee9e23493a0a6c906bc72750e62be
Maknoob <imak90@gmail.com>
2024-07-16 08:46:51 +0200

    refactored to the new embedded objects

--- model/src/main/java/com/codingmak/model/Weapon.java
+++ model/src/main/java/com/codingmak/model/Weapon.java
@@ -1,5 +1,6 @@
 package com.codingmak.model;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 
 @Entity
@@ -7,20 +8,18 @@ public class Weapon extends BaseItem {
 
     private double weight;
     private String skill;
-    private String requires;
-    private String scaling;
-    private int attackPhy;
-    private int attackMag;
-    private int attackFire;
-    private int attackLigt;
-    private int attackHoly;
-    private int attackCrit;
-    private int guardPhy;
-    private int guardMag;
-    private int guardFire;
-    private int guardLigt;
-    private int guardHoly;
-    private int guardBoost;
+
+    @Embedded
+    private Require require;
+
+    @Embedded
+    private Scaling scaling;
+
+    @Embedded
+    private Attack attack;
+
+    @Embedded
+    private Guard guard;
 
     public double getWeight() {
         return weight;
@@ -38,116 +37,36 @@ public class Weapon extends BaseItem {
         this.skill = skill;
     }
 
-    public String getRequires() {
-        return requires;
+    public Require getRequire() {
+        return require;
     }
 
-    public void setRequires(String requires) {
-        this.requires = requires;
+    public void setRequire(Require require) {
+        this.require = require;
     }
 
-    public String getScaling() {
+    public Scaling getScaling() {
         return scaling;
     }
 
-    public void setScaling(String scaling) {
+    public void setScaling(Scaling scaling) {
         this.scaling = scaling;
     }
 
-    public int getAttackPhy() {
-        return attackPhy;
+    public Attack getAttack() {
+        return attack;
     }
 
-    public void setAttackPhy(int attackPhy) {
-        this.attackPhy = attackPhy;
+    public void setAttack(Attack attack) {
+        this.attack = attack;
     }
 
-    public int getAttackMag() {
-        return attackMag;
+    public Guard getGuard() {
+        return guard;
     }
 
-    public void setAttackMag(int attackMag) {
-        this.attackMag = attackMag;
-    }
-
-    public int getAttackFire() {
-        return attackFire;
-    }
-
-    public void setAttackFire(int attackFire) {
-        this.attackFire = attackFire;
-    }
-
-    public int getAttackLigt() {
-        return attackLigt;
-    }
-
-    public void setAttackLigt(int attackLigt) {
-        this.attackLigt = attackLigt;
-    }
-
-    public int getAttackHoly() {
-        return attackHoly;
-    }
-
-    public void setAttackHoly(int attackHoly) {
-        this.attackHoly = attackHoly;
-    }
-
-    public int getAttackCrit() {
-        return attackCrit;
-    }
-
-    public void setAttackCrit(int attackCrit) {
-        this.attackCrit = attackCrit;
-    }
-
-    public int getGuardPhy() {
-        return guardPhy;
-    }
-
-    public void setGuardPhy(int guardPhy) {
-        this.guardPhy = guardPhy;
-    }
-
-    public int getGuardMag() {
-        return guardMag;
-    }
-
-    public void setGuardMag(int guardMag) {
-        this.guardMag = guardMag;
-    }
-
-    public int getGuardFire() {
-        return guardFire;
-    }
-
-    public void setGuardFire(int guardFire) {
-        this.guardFire = guardFire;
-    }
-
-    public int getGuardLigt() {
-        return guardLigt;
-    }
-
-    public void setGuardLigt(int guardLigt) {
-        this.guardLigt = guardLigt;
-    }
-
-    public int getGuardHoly() {
-        return guardHoly;
-    }
-
-    public void setGuardHoly(int guardHoly) {
-        this.guardHoly = guardHoly;
-    }
-
-    public int getGuardBoost() {
-        return guardBoost;
-    }
-
-    public void setGuardBoost(int guardBoost) {
-        this.guardBoost = guardBoost;
+    public void setGuard(Guard guard) {
+        this.guard = guard;
     }
 }
 
