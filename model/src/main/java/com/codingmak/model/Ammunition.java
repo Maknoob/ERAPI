89985d36b69f1486e54d7669d6b3d27f3385d7bc
Maknoob <imak90@gmail.com>
2024-07-16 09:21:06 +0200

    embedded the Attack Object

--- model/src/main/java/com/codingmak/model/Ammunition.java
+++ model/src/main/java/com/codingmak/model/Ammunition.java
@@ -1,67 +1,25 @@
 package com.codingmak.model;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Ammunition extends BaseItem {
-    private int attackPhy;
-    private int attackMag;
-    private int attackFire;
-    private int attackLigt;
-    private int attackHoly;
-    private int attackCrit;
     private String passive;
+
+    @Embedded
+    private Attack attack;
 
     public Ammunition() {
 
     }
 
-    public int getAttackPhy() {
-        return attackPhy;
+    public Attack getAttack() {
+        return attack;
     }
 
-    public void setAttackPhy(int attackPhy) {
-        this.attackPhy = attackPhy;
-    }
-
-    public int getAttackMag() {
-        return attackMag;
-    }
-
-    public void setAttackMag(int attackMag) {
-        this.attackMag = attackMag;
-    }
-
-    public int getAttackFire() {
-        return attackFire;
-    }
-
-    public void setAttackFire(int attackFire) {
-        this.attackFire = attackFire;
-    }
-
-    public int getAttackLigt() {
-        return attackLigt;
-    }
-
-    public void setAttackLigt(int attackLigt) {
-        this.attackLigt = attackLigt;
-    }
-
-    public int getAttackHoly() {
-        return attackHoly;
-    }
-
-    public void setAttackHoly(int attackHoly) {
-        this.attackHoly = attackHoly;
-    }
-
-    public int getAttackCrit() {
-        return attackCrit;
-    }
-
-    public void setAttackCrit(int attackCrit) {
-        this.attackCrit = attackCrit;
+    public void setAttack(Attack attack) {
+        this.attack = attack;
     }
 
     public String getPassive() {
