c5a3b272259ea8c33ae63a7f0dcb9a298b6743d5
Maknoob <imak90@gmail.com>
2024-07-16 12:01:03 +0200

    deleted requires and scaling method for search

--- repositories/src/main/java/com/codingmak/repositories/ShieldRepository.java
+++ repositories/src/main/java/com/codingmak/repositories/ShieldRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 public interface ShieldRepository extends JpaRepository<Shield, Long> {
     List<Shield> findByNameContaining(String name);
     List<Shield> findByTypeContaining(String type);
-    List<Shield> findByRequiresContaining(String requires);
-    List<Shield> findByScalingContaining(String scaling);
 }
