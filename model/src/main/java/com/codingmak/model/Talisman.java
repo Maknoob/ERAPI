65bcfde31440025c45789b2a91a1e8bbd8e3e9ff
Maknoob <imak90@gmail.com>
2024-07-16 08:45:29 +0200

    added location

--- model/src/main/java/com/codingmak/model/Talisman.java
+++ model/src/main/java/com/codingmak/model/Talisman.java
@@ -1,11 +1,23 @@
 package com.codingmak.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Talisman extends BaseItem {
     private double weight;
     private String effect;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
     public double getWeight() {
         return weight;
